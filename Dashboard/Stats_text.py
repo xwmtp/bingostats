03198c00c9745a82bcd15a363439d2577c0b9d48
xwmtp <sdstempher@ziggo.nl>
2019-05-20 17:02:22 +0200

    Fixed blank bug, changed blank/forfeit percs, make goals only appear when there are any.

--- Dashboard/Stats_text.py
+++ Dashboard/Stats_text.py
@@ -5,15 +5,15 @@ def get_stats_text(player, input):
     # overall stats
     all_races = player.select_races(type="bingo", forfeits=True)
     completed_races = player.select_races(type="bingo", forfeits=False)
-    num_blanks = len([r for r in completed_races if r.row == 'blank'])
+    num_blanks = len([r for r in completed_races if r.row_id == 'blank'])
     num_forfeits = len([r for r in all_races if r.forfeit])
 
 
     blank_perc   = round((num_blanks   / len(completed_races)) * 100, 1) if len(completed_races) > 0 else 0
     forfeit_perc = round((num_forfeits / len(all_races))       * 100, 1) if len(all_races)       > 0 else 0
 
-    blank_shame   = ' .shame' if blank_perc   > 30 else ''
-    forfeit_shame = ' .shame' if forfeit_perc > 30 else ''
+    blank_shame   = ' .shame' if blank_perc   > 25 else ''
+    forfeit_shame = ' .shame' if forfeit_perc > 25 else ''
 
 
     if player.name == '-1':
